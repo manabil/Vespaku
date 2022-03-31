5fcfd47e55dcf4b79fc02858a5021dad2f33d574
Pureism <pureismx@gmail.com>
2022-03-31 14:50:50 +0700

    Customizing Search and Profile Employee Page

--- resources/views/cari.blade.php
+++ resources/views/cari.blade.php
@@ -2,8 +2,20 @@
 
 <!-- ======= Breadcrumbs ======= -->
 @section('breadcrumbs')
-<footer id="footer" class="footer">
+<section class="breadcrumbs"  data-aos="zoom-out" data-aos-duration="1000">
+  <div class="container">
+    <ol>
+      <li><a href="/">Home</a></li>
+      <li><a href="#">Cari Pegawai</a></li>
+    </ol>
+    <h2>{{ $title }}</h2>
+  </div>
+</section><!-- End Breadcrumbs -->
+@endsection
 
+<!-- ======= Search Bar ======= -->
+@section('search_bar')
+<footer id="footer" class="footer">
   <div class="footer-newsletter">
     <div class="container" data-aos="fade-down" data-aos-duration="1000">
       <div class="row justify-content-center">
@@ -16,7 +28,6 @@
       </div>
     </div>
   </div>
-
 </footer>
 @endsection
 
@@ -34,71 +45,33 @@
         <article class="entry entry-single">
 
           <h2 class="entry-title">
-            <a href="blog-single.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</a>
+            <a href="blog-single.html">Daftar Pegawai</a>
           </h2>
 
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-              <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.html">12 Comments</a></li>
-            </ul>
-          </div>
-
-          <div class="entry-content">
-            <p>
-              Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-              Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
-            </p>
-
-            <p>
-              Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
-            </p>
-
-            <blockquote>
-              <p>
-                Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-              </p>
-            </blockquote>
-
-            <p>
-              Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-              Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-              Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
-            </p>
-
-            <h3>Et quae iure vel ut odit alias.</h3>
-            <p>
-              Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-              Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-              Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
-            </p>
-            <img src="template/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
-
-            <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
-            <p>
-              Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-              Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
-            </p>
-            <p>
-              Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
-            </p>
-
-          </div>
-
-          <div class="entry-footer">
-            <i class="bi bi-folder"></i>
-            <ul class="cats">
-              <li><a href="#">Business</a></li>
-            </ul>
-
-            <i class="bi bi-tags"></i>
-            <ul class="tags">
-              <li><a href="#">Creative</a></li>
-              <li><a href="#">Tips</a></li>
-              <li><a href="#">Marketing</a></li>
-            </ul>
-          </div>
+          <table class="table table-hover table-borderless entry-table ">
+            <thead>
+              <tr>
+                <th scope="col" class="no">No</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Nama Jabatan Terakhir</th>
+                <th scope="col">Jenis Jabatan Terakhir</th>
+                <th scope="col">NIP</th>
+                <th scope="col">Aksi</th>
+              </tr>
+            </thead>
+            @foreach ($pegawai as $no=>$p)
+              <tbody>
+                <tr>
+                  <th scope="row" class="no">{{ $no+1 }}</th>
+                  <td>{{ $p['nama'] }}</td>
+                  <td>{{ $p['jabatan'] }}</td>
+                  <td>{{ $p['jenis_jabatan'] }}</td>
+                  <td>{{ $p['nip'] }}</td>
+                  <td><a href="/cari/{{ $p['slug'] }}" class="btn btn-sm btn-outline-warning"><i class="bi bi-eye"></i>Lihat</a></td>
+                </tr>
+              </tbody>
+            @endforeach
+          </table>
 
         </article><!-- End blog entry -->
 
