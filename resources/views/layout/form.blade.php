396e8ce055893c9a90ecaa6fe1b0a652a835d47f
Pureism <pureismx@gmail.com>
2022-04-19 14:54:04 +0700

    Add Image Preview function

--- resources/views/layout/form.blade.php
+++ resources/views/layout/form.blade.php
@@ -66,8 +66,25 @@
   <script src="{{ asset('template/js/main.js') }}"></script>
 
   <script>
-    function button_logout() {
+    function button_logout() 
+    {
         document.getElementById("form-submit").submit();
+    }
+
+    function previewImage()
+    {
+      const image = document.querySelector('#foto')
+      const imgPreview = document.querySelector('.img-preview')
+
+      imgPreview.style.display = 'block'
+
+      const oFReader = new FileReader()
+      oFReader.readAsDataURL(image.files[0])
+      oFReader.onload = function(oFREvent)
+        {
+          imgPreview.src = oFREvent.target.result
+        }
+
     }
   </script>
 
