8063a083f624f824fa7553c9b4f9db77d615cbe1
Pureism <pureismx@gmail.com>
2022-05-09 14:43:26 +0700

    Add create method in RequestController

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PangkatUser;
 use Illuminate\Http\Request;
 use App\Models\Request as RequestModel;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class RequestController extends Controller
 {
-    public function index(){
-        $request = RequestModel::where('owner', auth()->user()->id)->get();
-        $user = User::with(['jabatan', 'pangkat', 'request']);
+    public function index(User $user, PangkatUser $slug){
+        $get_request = RequestModel::where('owner', auth()->user()->id)->get();
+        $set_request = RequestModel::where('user_id', auth()->user()->id)->get();
+        $username = User::all();
+        $token = Str::random(20);
 
         return view('request', [
             'title' => 'Manajemen Request',
-            'requests' => $request,
+            'get_requests' => $get_request,
+            'set_requests' => $set_request,
+            'username' => $username,
+            'token' => $token
         ]);
+    }
+
+    public function create(){
+
+    }
+
+    public function accept(Request $request){
+        RequestModel::create($request);
+        return redirect('/request')->with('request_accepted', 'Request diterima');
+    }
+    
+    public function reject(Request $request){
+        RequestModel::create($request);
+        return redirect('/request')->with('request_rejected', 'Request ditolak');
     }
 }
