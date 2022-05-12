b396e064f648f185b967335e1b49971f25f64e59
Pureism <pureismx@gmail.com>
2022-05-12 14:46:06 +0700

    Add paginate, Modifying update, store Request Controller

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -14,15 +14,15 @@ class RequestController extends Controller
 {
     public function index()
     {
-        $get_request = RequestModel::where('owner', auth()->user()->id)->get();
-        $set_request = RequestModel::where('user_id', auth()->user()->id)->get();
+        $get_request = RequestModel::with(['user'])->where('owner', auth()->user()->id)->whereNotIn('aksi', ['terima', 'tolak'])->latest('tanggal_aksi');
+        $set_request = RequestModel::with(['user'])->where('user_id', auth()->user()->id)->latest('tanggal_aksi');
         $username = User::all();
         $token = Str::random(20);
 
         return view('request.index', [
             'title' => 'Manajemen Request',
-            'get_requests' => $get_request,
-            'set_requests' => $set_request,
+            'get_requests' => $get_request->paginate(5, ['*'], 'get_request')->withQueryString(),
+            'set_requests' => $set_request->paginate(5, ['*'], 'set_request')->withQueryString(),
             'username' => $username,
             'token' => $token
         ]);
@@ -38,7 +38,7 @@ class RequestController extends Controller
         ]);
     }
 
-    public function store(PangkatUser $pangkat, JabatanUser $jabatan, Request $request)
+    public function store(Request $request)
     {
         $request['tanggal_aksi'] = now();
 
@@ -46,13 +46,16 @@ class RequestController extends Controller
         return redirect('/request')->with('request_added', 'Request Berhasil Dibuat');
     }
 
-    public function update(Request $request, PangkatUser $pangkat)
+    public function update(Request $request)
     {
         $updated_request = array_slice($request->toArray(), 1, count($request->toArray()));
         $updated_request['tanggal_aksi'] = now();
 
         RequestModel::where('id', $request->id)->update($updated_request);
-        return redirect('/request')->with('request_accepted', 'Request diterima');
+        if ($updated_request['aksi'] === 'terima') {
+            return redirect('/request')->with('request_accepted', 'Request berhasil diterima');
+        }
+        return redirect('/request')->with('request_rejected', 'Request berhasil ditolak');
     }
 
     public function token(PangkatUser $pangkat, JabatanUser $jabatan, Request $request)
@@ -60,7 +63,23 @@ class RequestController extends Controller
         return view('request.token', [
             'title' => 'Unduh File',
             'pangkat' => $pangkat,
+            'jabatan' => $jabatan,
             'username' => $request->username,
         ]);
+    }
+
+    public function download(PangkatUser $pangkat, JabatanUser $jabatan, Request $request)
+    {
+        $validatedData = $request->validate([
+            'token' => 'required|min:20|max:20',
+        ]);
+
+        if ($pangkat->token !== $validatedData['token']) {
+            return 'TOken ndak sama';
+        }
+        if (!$pangkat->is_downloaded) {
+            return 'Token kadaluarsa';
+        }
+        return 'silahkan download';
     }
 }
